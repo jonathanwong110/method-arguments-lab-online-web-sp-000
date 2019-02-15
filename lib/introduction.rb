f4b6b51384ff537bb5deeca06dc622bfc870e5ea
Jonathan Wong <jonathan.wong110@gmail.com>
2019-02-15 17:06:23 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+def introduction(language)
+  puts "Hi, my name is Dan and I am learning to program in #{language}."
 end
